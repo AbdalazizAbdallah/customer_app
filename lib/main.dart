8a6317d4395821ac98549038514c62c951735d47
Abdalaziz Abdallah <abdalazizabdalluh3@gmail.com>
2022-09-11 22:16:11 +0300

    style : set size screens

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      designSize: const Size(375, 667),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (_, child) {
