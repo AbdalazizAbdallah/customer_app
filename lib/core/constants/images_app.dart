dad2596c64a59275541258fb6e755ef8a866ab04
Abdalaziz Abdallah <abdalazizabdalluh3@gmail.com>
2022-09-11 22:37:15 +0300

    style : initial branch for onboarding

--- lib/core/constants/images_app.dart
+++ lib/core/constants/images_app.dart
@@ -1,7 +1,10 @@
 class ImagesApp {
   static const String _imagePath = 'assets/images/';
   static const String _svgPath = 'assets/svg/';
+  static const String _jsonPath = 'assets/json_image/';
   static const String backgroundSplashPath =
       '${_imagePath}backgroundSplash.png';
   static const String wholeLogoPath = '${_svgPath}white_whole_logo.svg';
+
+  static const String orderFood = '${_jsonPath}119398-order-food.json';
 }
