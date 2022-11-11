35b8e30eafd66f98e6e7ebcde57bb43c7ae4278a
Abdalaziz Abadallah <abdalazizabdalluh3@gmail.com>
2022-11-11 10:29:47 +0200

    finish from delivery bottom sheet

--- lib/ui/pages/checkout/widgets/select_delivery_time_bottom_sheet.dart
+++ lib/ui/pages/checkout/widgets/select_delivery_time_bottom_sheet.dart
@@ -4,6 +4,7 @@ import 'package:customer_app/core/constants/colors_app.dart';
 import 'package:customer_app/core/constants/sizes_app.dart';
 import 'package:customer_app/core/constants/styles_app.dart';
 import 'package:customer_app/core/widgets/custom_button_widget.dart';
+import 'package:customer_app/ui/pages/checkout/controllers/delivery_time_bottom_controller.dart';
 import 'package:customer_app/ui/pages/checkout/widgets/triangle_pointer.dart';
 import 'package:customer_app/ui/widgets/header_dialogs_widget.dart';
 import 'package:flutter/cupertino.dart';
@@ -35,27 +36,108 @@ class SelectDeliveryTimeBottomSheet extends StatelessWidget {
               ),
             ),
           ),
-          Flexible(
+          Expanded(
             flex: 1,
             child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: SizesApp.r30,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    SelectableText(
-                      'Today',
-                      textAlign: TextAlign.center,
-                      style: StylesApp.headline6,
-                    ),
-                    SelectableText(
-                      'Towmoro',
-                      textAlign: TextAlign.center,
-                      style: StylesApp.headline6,
-                    )
-                  ],
-                )),
+              padding: EdgeInsets.symmetric(
+                horizontal: SizesApp.r30,
+              ),
+              child: GetBuilder<DeliveryTimeBottomController>(
+                  init: DeliveryTimeBottomController(),
+                  builder: (deliveryTimeBottomController) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Expanded(
+                          child: InkWell(
+                            splashColor: null,
+                            onTap: () {
+                              deliveryTimeBottomController.makeTodayActive();
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  'Today',
+                                  textAlign: TextAlign.center,
+                                  style: StylesApp.headline6.copyWith(
+                                    color: deliveryTimeBottomController
+                                        .todayTextColor,
+                                    fontWeight: deliveryTimeBottomController
+                                                .currentSelectText ==
+                                            TextSelectionEnum.todayText
+                                        ? FontWeight.bold
+                                        : FontWeight.normal,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: SizesApp.r16),
+                                  child: SizedBox(
+                                    height: SizesApp.r2,
+                                    width: SizesApp.doubleInfinity,
+                                    child: DecoratedBox(
+                                      decoration: BoxDecoration(
+                                        color: deliveryTimeBottomController
+                                            .todayTextColor,
+                                        borderRadius: BorderRadius.circular(
+                                          SizesApp.r2,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: InkWell(
+                            splashColor: null,
+                            onTap: () {
+                              deliveryTimeBottomController.makeTomorrowActive();
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  'Towmoro',
+                                  textAlign: TextAlign.center,
+                                  style: StylesApp.headline6.copyWith(
+                                    color: deliveryTimeBottomController
+                                        .tomorrowTextColor,
+                                    fontWeight: deliveryTimeBottomController
+                                                .currentSelectText ==
+                                            TextSelectionEnum.tomorrowText
+                                        ? FontWeight.bold
+                                        : FontWeight.normal,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: SizesApp.r16),
+                                  child: SizedBox(
+                                    height: SizesApp.r2,
+                                    width: SizesApp.doubleInfinity,
+                                    child: DecoratedBox(
+                                      decoration: BoxDecoration(
+                                        color: deliveryTimeBottomController
+                                            .tomorrowTextColor,
+                                        borderRadius: BorderRadius.circular(
+                                          SizesApp.r2,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    );
+                  }),
+            ),
           ),
           Expanded(
             flex: 2,
